469f3addc0548a0907b3af622f1193428125ccd8
cycodedconcept <cycodedconcept@gmail.com>
2023-02-17 08:50:22 +0100

    animation for the home page

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showit');
+        }else {
+            entry.target.classList.remove('showit');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el))
