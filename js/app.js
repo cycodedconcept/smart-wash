f3d81e8e1e2c6284acb9930f8c468efd53fbe671
cycodedconcept <cycodedconcept@gmail.com>
2023-02-27 12:46:14 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('showit');
-        }else {
-            entry.target.classList.remove('showit');
-        }
-    });
+        setTimeout(() => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('showit');
+            }
+
+            else {
+                entry.target.classList.remove('showit');
+            }
+        }, 4000)
+    }, {threshold:[1.5]});
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
