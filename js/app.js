d29fefba493f31ce03a1fac55683a6def16b6b92
cycodedconcept <cycodedconcept@gmail.com>
2023-03-30 06:59:28 +0100

    signup page

--- js/app.js
+++ js/app.js
@@ -1,18 +1,54 @@
-// const observer = new IntersectionObserver((entries) => {
-//     entries.forEach((entry) => {
-//         console.log(entry)
-//         setTimeout(() => {
-//             if (entry.isIntersecting) {
-//                 entry.target.classList.add('showit');
-//             }
+const baseUrl = "https://rose-brainy-sparrow.cyclic.app";
 
-//             else {
-//                 entry.target.classList.remove('showit');
-//             }
-//         }, 4000)
-//     }, {threshold:[1.5]});
-// });
+function signup(event) {
+    event.preventDefault();
 
-// const hiddenElements = document.querySelectorAll('.hidden');
-// hiddenElements.forEach((el) => observer.observe(el))
+    const getspin = document.querySelector("#spin");
+    getspin.style.display = "inline-block";
 
+    const getName = document.getElementById("name").value;
+    const getEmail = document.getElementById("email").value;
+    const getPhone = document.getElementById("phone").value;
+    const getPassword = document.getElementById("password").value;
+    const getPolicy = document.getElementById("policy").value;
+
+    if (getName === "" || getEmail === "" || getPhone === "" || getPassword === "" || getPolicy === "") {
+        Swal.fire({
+            icon: 'info',
+            text: 'All fields are required!',
+            confirmButtonColor: '#00AEEF'
+        })
+    }
+
+    else {
+        let person = {
+            name: getName,
+            email: getEmail,
+            phone: getPhone,
+            password: getPassword,
+            policy: getPolicy
+        };
+
+        localStorage.setItem("users", JSON.stringify(person));
+        // let headers = new Headers();
+        // headers.append('Content-Type', 'application/json');
+        // headers.append('Accept', 'application/json');
+        // headers.append('Origin','http://127.0.0.1:5501/');
+
+        const signReq = {
+            // mode: 'cors',
+            method: 'GET',
+            // credentials: 'include',
+            // headers: headers
+        }
+
+        const url = `${baseUrl}/api/user/sendotp/` + `${getEmail}`;
+        fetch(url, signReq)
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+
+    }
+
+
+}
