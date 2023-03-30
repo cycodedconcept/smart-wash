67970c8f20cfabee5349693591e04bf5648b1c66
cycodedconcept <cycodedconcept@gmail.com>
2023-03-30 19:41:44 +0100

    signup

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-const baseUrl = "https://rose-brainy-sparrow.cyclic.app";
+
+let baseUrl = "https://washsmart.onrender.com";
 
 function signup(event) {
     event.preventDefault();
@@ -30,25 +31,67 @@ function signup(event) {
         };
 
         localStorage.setItem("users", JSON.stringify(person));
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        headers.append('Accept', 'application/json');
-        headers.append('Origin','http://127.0.0.1:5501/');
 
         const signReq = {
-            mode: 'cors',
+            mode: 'no-cors',
             method: 'GET',
-            credentials: 'include',
-            headers: headers
         }
 
-        const url = `${baseUrl}/api/user/sendotp/` + `${getEmail}`;
+        let url = `${baseUrl}/api/user/sendotp/` + `${getEmail}`
         fetch(url, signReq)
-        .then(response => response.json())
+        .then(response => {
+            // return response.json()
+            responseClone = response.clone();
+            return response.json();
+        })
         .then(result => console.log(result))
         .catch(error => console.log('error', error));
 
     }
 
+}
 
+function getPricing() {
+    var request = {
+        mode: 'no-cors',
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    let url = `${baseUrl}/api/pricing/get_pricing`;
+    fetch(url, request)
+    .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+
+}
+getPricing();
+
+
+function googleSignIn(event) {
+    event.preventDefault();
+
+    // let headers = new Headers();
+    // headers.append('Content-Type', 'application/json');
+
+    // const googleIt = {
+    //     mode: 'no-cors',
+    //     method: 'GET',
+    //     headers: headers
+    // }
+
+    // const url = `${baseUrl}/api/user`;
+    // fetch(url, googleIt)
+    // .then(response => response.text())
+    // .then(result => {
+    //     console.log(result)
+    //     localStorage.setItem("user2", JSON.stringify(result))
+    // })
+    // .catch(error => console.log('error', error));
+    document.cookie = "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&prompt=consent&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&response_type=code&client_id=454924869328-m14q0u1deaca3b921vm69jevpth1tv1m.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fapi%2Fuser%2Fgoogle%2Fauth";
+    setTimeout(() => {
+    location.href = x;
+    }, 2000);
+
+    let x = document.cookie;
 }
