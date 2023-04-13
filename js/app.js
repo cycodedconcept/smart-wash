76ae51211f01d67e105489176eaa18557a1fb672
cycodedconcept <cycodedconcept@gmail.com>
2023-04-13 09:14:46 +0100

    update admin password

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 
 let baseUrl = "https://washsmart.onrender.com";
-document.cookie = "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&prompt=consent&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&response_type=code&client_id=454924869328-m14q0u1deaca3b921vm69jevpth1tv1m.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fapi%2Fuser%2Fgoogle%2Fauth";
-let x = document.cookie;
+// document.cookie = "https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&prompt=consent&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email&response_type=code&client_id=454924869328-m14q0u1deaca3b921vm69jevpth1tv1m.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Fapi%2Fuser%2Fgoogle%2Fauth";
+// let x = document.cookie;
 
 function toLogin(event) {
     event.preventDefault();
@@ -58,7 +58,7 @@ function signup(event) {
         .then(response => response.json())
         .then(result => {
             console.log(result)
-            localStorage.setItem("user3", JSON.stringify(result))
+            localStorage.setItem("user3", JSON.stringify(result));
             if (result.message === "Email Sent") {
                 location.href = `otp.html?email=${getEmail}`
             }
@@ -97,58 +97,68 @@ function getOtp(event) {
     const tn = document.querySelector(".ba3").value;
     const fon = document.querySelector(".ba4").value;
 
-    const otp = fn + sn + tn + fon;
-
-    const getHash = localStorage.getItem("user3");
-    const hashIt = JSON.parse(getHash);
-    const hash = hashIt.encrypted_data;
-
-    const getEmail = localStorage.getItem("users");
-    const hashEmail = JSON.parse(getEmail);
-    const hash2 = hashEmail.email;
-
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-
-    const raw = JSON.stringify({
-        "otp": otp,
-        "hash": hash,
-        "email": hash2
-    });
-
-    const otpReq = {
-        method: 'POST',
-        body: raw,
-        headers: myHeaders
+    if (fn === "" || sn === "" || tn === "" || fon === "") {
+        Swal.fire({
+            icon: 'info',
+            text: 'All fields are required!',
+            confirmButtonColor: '#00AEEF'
+        })
+        getSpin.style.display = "none"
     }
+    else {
+        const otp = fn + sn + tn + fon;
 
-    const url = `${baseUrl}/api/user/verify-otp/`;
-    fetch(url, otpReq)
-    .then(response => response.json())
-    .then(result => {
-        console.log(result)
+        const getHash = localStorage.getItem("user3");
+        const hashIt = JSON.parse(getHash);
+        const hash = hashIt.encrypted_data;
 
-        if (result.message === "Valid") {
-            location.href = "../pages/success.html"
+        const getEmail = localStorage.getItem("users");
+        const hashEmail = JSON.parse(getEmail);
+        const hash2 = hashEmail.email;
+
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "otp": otp,
+            "hash": hash,
+            "email": hash2
+        });
+
+        const otpReq = {
+            method: 'POST',
+            body: raw,
+            headers: myHeaders
         }
-        else {
+
+        const url = `${baseUrl}/api/user/verify-otp/`;
+        fetch(url, otpReq)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+
+            if (result.message === "Valid") {
+                location.href = "../pages/success.html"
+            }
+            else {
+                Swal.fire({
+                    icon: 'info',
+                    text: 'Unsuccessful please try again',
+                    confirmButtonColor: '#00AEEF'
+                })
+                getSpin.style.display = "none";
+            }
+        })
+        .catch(error => {
+            console.log('error', error)
             Swal.fire({
                 icon: 'info',
-                text: 'Unsuccessful please try again',
+                text: error,
                 confirmButtonColor: '#00AEEF'
             })
             getSpin.style.display = "none";
-        }
-    })
-    .catch(error => {
-        console.log('error', error)
-        Swal.fire({
-            icon: 'info',
-            text: error,
-            confirmButtonColor: '#00AEEF'
-        })
-        getSpin.style.display = "none";
-    });
+        }); 
+    }
 
 }
 
@@ -319,11 +329,17 @@ function dashboardApi() {
     .then(response => response.json())
     .then(result => {
         console.log(result)
+        localStorage.setItem("dash", JSON.stringify(result))
         let removeBtn = document.querySelector(".sign-btn");
         let formParent = document.querySelector(".form-inline");
 
+        // const getEmail = document.getElementById("adminEmail");
+        // getEmail.setAttribute('value', `${result.user.email}`);
+        // getEmail.style.color = "#00AEEF"
 
-        if (result.user.hasOwnProperty("email")) {
+
+
+        if (result.user.hasOwnProperty('email')) {
             removeBtn.style.display = "none"
 
             const newDiv = document.createElement("div");
@@ -355,9 +371,196 @@ function dashboardApi() {
 }
 dashboardApi();
 
+
+
+
+// function getAdminEmail(event) {
+//     event.preventDefault();
+
+//     const getSpin = document.querySelector("#spin");
+//     getSpin.style.display = "inline-block";
+
+//     const getEmail = document.getElementById("adminEmail").value;
+
+//     const signReq = {
+//         method: 'GET',
+//     }
+
+//     let url = `${baseUrl}/api/user/sendotp/` + `${getEmail}`;
+//     fetch(url, signReq)
+//     .then(response => response.json())
+//     .then(result => {
+//         console.log(result)
+//         localStorage.setItem("userAdmin", JSON.stringify(result));
+//         if (result.message === "Email Sent") {
+//             location.href = `otp2.html?email=${getEmail}`
+//         }
+//         else {
+//             Swal.fire({
+//                 icon: 'info',
+//                 text: 'Unsuccessful please try again',
+//                 confirmButtonColor: '#00AEEF'
+//             })
+//             getspin.style.display = "none";
+//         }
+//     })
+//     .catch(error => {
+//         console.log('error', error)
+//         Swal.fire({
+//             icon: 'info',
+//             text: error,
+//             confirmButtonColor: '#00AEEF'
+//         })
+//         getspin.style.display = "none";
+//     });
+    
+// }
+
+// function sendOtp(event) {
+//     event.preventDefault();
+
+//     const getSpin = document.querySelector("#spin");
+//     getSpin.style.display = "inline-block";
+
+//     const fn = document.querySelector(".ba").value;
+//     const sn = document.querySelector(".ba2").value;
+//     const tn = document.querySelector(".ba3").value;
+//     const fon = document.querySelector(".ba4").value;
+
+//     if (fn === "" || sn === "" || tn === "" || fon === "") {
+//         Swal.fire({
+//             icon: 'info',
+//             text: 'All fields are required!',
+//             confirmButtonColor: '#00AEEF'
+//         })
+//         getSpin.style.display = "none"
+//     }
+
+//     else {
+//         const otp = fn + sn + tn + fon;
+
+//         const getHash = localStorage.getItem("userAdmin");
+//         const hashIt = JSON.parse(getHash);
+//         const hash = hashIt.encrypted_data;
+
+//         const getEmail = localStorage.getItem("dash");
+//         const hashEmail = JSON.parse(getEmail);
+//         const hash2 = hashEmail.email;
+
+//         const myHeaders = new Headers();
+//         myHeaders.append("Content-Type", "application/json");
+
+//         const raw = JSON.stringify({
+//             "otp": otp,
+//             "hash": hash,
+//             "email": hash2
+//         });
+
+//         const otpReq = {
+//             method: 'POST',
+//             body: raw,
+//             headers: myHeaders
+//         }
+
+//        const url = `${baseUrl}/api/user/verify-otp/`;
+//        fetch(url, otpReq)
+//        .then(response => response.json())
+//         .then(result => {
+//             console.log(result)
+
+//             if (result.message === "Valid") {
+//                 location.href = "../pages/update_password_admin.html"
+//             }
+//             else {
+//                 Swal.fire({
+//                     icon: 'info',
+//                     text: 'Unsuccessful please try again',
+//                     confirmButtonColor: '#00AEEF'
+//                 })
+//                 getSpin.style.display = "none";
+//             }
+//         })
+//         .catch(error => {
+//             console.log('error', error)
+//             Swal.fire({
+//                 icon: 'info',
+//                 text: error,
+//                 confirmButtonColor: '#00AEEF'
+//             })
+//             getSpin.style.display = "none";
+//         });
+//     }
+// }
+
+function updatePasswordAdmin(event) {
+    event.preventDefault();
+
+    const getSpin = document.getElementById("spin");
+    getSpin.style.display = "inline-block";
+
+    const getToken = localStorage.getItem("token");
+    const myToken = JSON.parse(getToken);
+
+    const oldPass = document.getElementById("oldpass").value;
+    const newPass = document.getElementById("newPass").value;
+    const confirmOldPass = document.getElementById("confirmNewPass").value;
+
+    if (oldPass === "" || newPass === "" || confirmOldPass === "") {
+        Swal.fire({
+            icon: 'info',
+            text: 'All fields are required!',
+            confirmButtonColor: '#00AEEF'
+        })
+        getSpin.style.display = "none";
+    }
+    if ( confirmOldPass !== newPass) {
+        Swal.fire({
+            icon: 'info',
+            text: 'Passwords do not match',
+            confirmButtonColor: '#00AEEF'
+        })
+        getSpin.style.display = "none";
+    }
+    
+    else {
+        const getHeader = new Headers();
+        getHeader.append("Authorization", `Bearer ${myToken}`);
+
+        const getBody = JSON.stringify({
+            "oldpassword": oldPass,
+            "newpassword": newPass,
+            "confirmpassword": confirmOldPass
+        })
+
+        const upReq = {
+            method: "POST",
+            headers: getHeader,
+            body: getBody
+        };
+
+        const url = `${baseUrl}/api/user/update-password`;
+        fetch(url, upReq)
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    }
+}
+
 function googleSignIn(event) {
     event.preventDefault();
-    location.href = x;
+
+    const goReq = {
+        method: 'GET',
+    }
+
+    const url = `${baseUrl}/api/user`;
+    fetch(url, goReq)
+    .then(response => response.json())
+    .then(result => {
+        console.log(result)
+        location.href = `${result.message}`;
+    })
+    .catch(error => console.log('error', error));
 }
 
 function getPricing() {
